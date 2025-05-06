2efb0bc4ad3165bc853eb2d7a57743b0f20340d3
Madusalves <malvesduarte444@gmail.com>
2025-05-06 16:16:32 -0300

    refactor: enhance code comments and remove outdated README.md

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,10 @@ import Header from "./components/Header";
 import Main from "./components/Main";
 
 const App = () => {
-  return (
+  return (  // The main App component that serves as the entry point for the application
     <>
-      <Header />
-      <Main />
+      <Header /> {/* Header component that likely contains the navigation and branding */}  
+      <Main />  {/* Main component that contains the main content of the application */}
     </>
   );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import { StrictMode } from "react"; // Importing React's StrictMode for highlighting potential problems in an application
+import { createRoot } from "react-dom/client"; // Importing createRoot from react-dom/client for rendering React components
 import "./index.css";
-import App from "./App.jsx";
+import App from "./App.jsx"; // Importing the main App component
 
+// Importing the main App component and rendering it into the root element of the HTML document
+// The createRoot function is used to create a root for the React application
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
+  <StrictMode>  {/* Wrapping the application in StrictMode to enable additional checks and warnings for its descendants*/}
     <App />
   </StrictMode>
 );
